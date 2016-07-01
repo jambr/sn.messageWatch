3bf99ff538ff8e7e7d63e25112e11d05c6a23c66
Karl Stoney <kstoney@thoughtworks.com>
2016-07-01 21:15:43 +0100

    Updated stocknet.topic to sn:topic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 let Broker = require('sn.core').Default.Broker;
-let broker = new Broker('stocknet:topic');
+let broker = new Broker('sn:topic');
 
 broker.subscribe(['sn.*', 'sn.*.*', 'sn.*.*.*'], () => {
 }, () => {
